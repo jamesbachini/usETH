f7bad8e1f3e8d7b2772a6b6a94ea2bcb11effefc
James Bachini <james@mcambridge.com>
2022-05-16 08:08:45 +0100

    Uniswap router weth > usdc

--- contracts/usETH.sol
+++ contracts/usETH.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
 interface ILido {
 	function submit(address _referral) external payable returns (uint256 StETH);
@@ -28,30 +30,62 @@ interface EACAggregatorProxy {
 }
 
 contract usETH is ERC20 {
-	address lidoAddress;
-	address aaveAddress;
-	address chainlinkAddress;
-	address usdcAddress;
-	address wethAddress;
+	address public lidoAddress;
+	address public aaveAddress;
+	address public chainlinkAddress;
+	address public uniswapAddress;
+	address public usdcAddress;
+	address public wethAddress;
 
-	constructor(address _lidoAddress, address _aaveAddress, address _chainlinkAddress, address _usdcAddress, address _wethAddress) ERC20("USD  Ether", "usETH") {
+	constructor(address _lidoAddress, address _aaveAddress, address _chainlinkAddress, address _uniswapAddress, address _usdcAddress, address _wethAddress) ERC20("USD  Ether", "usETH") {
 		lidoAddress = _lidoAddress;
 		aaveAddress = _aaveAddress;
 		chainlinkAddress = _chainlinkAddress;
+		uniswapAddress = _uniswapAddress;
 		usdcAddress = _usdcAddress;
 		wethAddress = _wethAddress;
 	}
+
+	function swap(uint256 _amountIn) internal returns (uint256) {
+		uint24 poolFee = 500;
+		ISwapRouter.ExactInputSingleParams memory params =
+			ISwapRouter.ExactInputSingleParams({
+				tokenIn: wethAddress,
+				tokenOut: usdcAddress,
+				fee: poolFee,
+				recipient: address(this),
+				deadline: block.timestamp,
+				amountIn: _amountIn,
+				amountOutMinimum: 0,
+				sqrtPriceLimitX96: 0
+			});
+		uint256 amountOut = ISwapRouter(uniswapAddress).exactInputSingle(params);
+		return amountOut;
+	}
+
 
 	function deposit() payable public {
 		uint256 stEthCollateral = ILido(lidoAddress).submit{value: msg.value}(address(this));
 		ILido(lidoAddress).approve(aaveAddress, stEthCollateral);
 		IAave(aaveAddress).deposit(lidoAddress,stEthCollateral,address(this),0);
 		int256 ethPriceInt = EACAggregatorProxy(chainlinkAddress).latestAnswer();
-		uint256 ethPrice = uint256(ethPriceInt) / 10e7;
-		uint256 usdAmount = stEthCollateral * ethPrice / 10e11 / 2; // 50% collateral to loan - 75% liquidation
-		IAave(aaveAddress).borrow(usdcAddress,usdAmount,2,0,address(this));
-		// Set up short position
-		_mint(msg.sender, msg.value); // 1 ETH = 1 usETH
+		uint256 ethDollarPrice = uint256(ethPriceInt) / 10e7;
+		uint256 usdEthValue = stEthCollateral * ethDollarPrice / 10e11;
+		uint256 borrowAmount = stEthCollateral * 7 / 10; // 70% collateral to loan - 75% liquidation
+		uint256 secondBorrow = stEthCollateral - borrowAmount; // leaves 30% remaining
+		IAave(aaveAddress).borrow(wethAddress,borrowAmount,2,0,address(this));
+		uint256 approveAllAtOnce = borrowAmount * 2;
+		IERC20(wethAddress).approve(uniswapAddress,approveAllAtOnce);
+		uint256 usdcBack = swap(borrowAmount);
+		IERC20(usdcAddress).approve(aaveAddress,usdcBack);
+		IAave(aaveAddress).deposit(usdcAddress,usdcBack,address(this),0);
+		IAave(aaveAddress).borrow(wethAddress,secondBorrow,2,0,address(this));
+		uint256 usdcBackAgain = swap(secondBorrow); // already approved
+		IERC20(usdcAddress).approve(aaveAddress,usdcBackAgain);
+		IAave(aaveAddress).deposit(usdcAddress,usdcBackAgain,address(this),0);
+		// 2do add check to see if usdcBackAgain price is lower than usdEthValue to avoid MEV/arbitrage
+		uint256 amountToMint = usdEthValue * 1e18;
+		_mint(msg.sender, amountToMint);
 	}
 
 	function withdraw() public {
--- test/1-contract1-spec.js
+++ test/1-contract1-spec.js
@@ -3,22 +3,18 @@ const { ethers } = require('hardhat');
 const lidoAbi = require('./../abis/lido.json');
 const aaveAbi = require('./../abis/aave.json');
 const usdcAbi = require('./../abis/usdc.json');
-/*
-// Rinkeby
-const lidoAddress = '0xF4242f9d78DB7218Ad72Ee3aE14469DBDE8731eD'; // https://docs.lido.fi/deployed-contracts
-const aaveAddress = '0x87530ED4bd0ee0e79661D65f8Dd37538F693afD5'; // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
-const usdcAddress = '0x5b8b635c2665791cf62fe429cb149eab42a3ced8';
-const dydxAddress = '0x4EC3570cADaAEE08Ae384779B0f3A45EF85289DE'; // not right
-*/
+const wethAbi = require('./../abis/weth.json');
+
 // Mainnet
 const lidoAddress = '0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84';
 const aaveAddress = '0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9';
-const usdcAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 const chainlinkAddress = '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419';
-const dydxAddress = '0x4EC3570cADaAEE08Ae384779B0f3A45EF85289DE';
+const uniswapAddress = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
+const usdcAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+const wethAddress = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 
-describe('btETH', function () {
-  let btETH, lido, aave, dydx;
+describe('usETH', function () {
+  let usETH, lido, aave, weth;
 
   before(async () => {
     [owner,user1] = await ethers.getSigners();
@@ -27,41 +23,42 @@ describe('btETH', function () {
     const ownerBalance = await ethers.provider.getBalance(owner.address);
     console.log(`    Owner: ${owner.address} Balance: ${ethers.utils.formatEther(ownerBalance)} ETH`);
     await hre.run('compile');
-    const btETHContract = await ethers.getContractFactory('btETH');
-    btETH = await btETHContract.deploy(lidoAddress,aaveAddress,usdcAddress,chainlinkAddress,dydxAddress);
-    await btETH.deployed();
-    console.log(`    btETH deployed to: ${btETH.address}`);
+    const usETHContract = await ethers.getContractFactory('usETH');
+    usETH = await usETHContract.deploy(lidoAddress,aaveAddress,chainlinkAddress,uniswapAddress,usdcAddress,wethAddress);
+    await usETH.deployed();
+    console.log(`    usETH deployed to: ${usETH.address}`);
     lido = new ethers.Contract(lidoAddress, lidoAbi, ethers.provider);
     aave = new ethers.Contract(aaveAddress, aaveAbi, ethers.provider);
     usdc = new ethers.Contract(usdcAddress, usdcAbi, ethers.provider);
+    weth = new ethers.Contract(wethAddress, wethAbi, ethers.provider);
   });
 
   it('LIDO contract is working', async function () {
-    const tx1 = await lido.balanceOf(btETH.address);
+    const tx1 = await lido.balanceOf(usETH.address);
     expect(tx1).to.be.eq(0);
     const tx2 = await lido.totalSupply();
     expect(tx2).to.be.gt(0);
   });
 
   it('Deposit ETH', async function () {
-    const tx1 = await btETH.deposit({value: ethers.utils.parseEther('0.01')});
+    const tx1 = await usETH.deposit({value: ethers.utils.parseEther('0.01')});
   });
 
-  it('Check stETH balance on btETH contract', async function () {
-    const tx1 = await lido.balanceOf(btETH.address);
+  it('Check stETH balance on usETH contract', async function () {
+    const tx1 = await lido.balanceOf(usETH.address);
     expect(tx1).to.be.gt(0);
   });
 
-  it('Check btETH balance for owner', async function () {
-    const btEthBalance = await btETH.balanceOf(owner.address);
-    expect(btEthBalance).to.be.gt(0);
+  it('Check usETH balance for owner', async function () {
+    const usEthBalance = await usETH.balanceOf(owner.address);
+    expect(usEthBalance).to.be.gt(0);
   });
 
-  it('Check USDC balance for btETH contract', async function () {
+  it('Check USDC balance for usETH contract', async function () {
       //const accountData = await aave.getUserAccountData(owner.address);
       //console.log(accountData);
-      const usdcBalance = await usdc.balanceOf(btETH.address);
-      expect(usdcBalance).to.be.gt(0);
+      const usdcBalance = await usdc.balanceOf(usETH.address);
+      expect(usdcBalance).to.be.eq(0);
   });
 
 
